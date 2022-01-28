d94f60a8d11b277e441702d3965cf8d45c9f5cc0
JiyeonMin <ichbinmin2@gmail.com>
2022-01-28 21:26:33 +0900

    Add : 상품 정보 고시 form input 값 생성

--- src/components/infoNotice/InfoNotice.jsx
+++ src/components/infoNotice/InfoNotice.jsx
@@ -2,55 +2,26 @@ import React, { useState } from "react";
 import InfoNoticeForm from "./infoNoticeForm/InfoNoticeForm";
 import { PALLETS } from "style/theme";
 import styled from "styled-components";
-
-const infoDic = [
-  {
-    id: 1,
-    title: "제품명 / 중량",
-    width: "600px",
-    content: "제품명/중량을 입력해 주세요.",
-  },
-  {
-    id: 2,
-    title: "원산지 / 원재료 함량",
-    width: "600px",
-    content: "원산지/원재 함량을 입력해 주세요.",
-  },
-  {
-    id: 3,
-    title: "등급",
-    width: "600px",
-    content: "등급 (근내지방도 수치)를 입력해 주세요.",
-  },
-  {
-    id: 4,
-    title: "보관",
-    width: "600px",
-    content: "보관 방식을 입력해주세요",
-  },
-  {
-    id: 5,
-    title: "식품 유형",
-    width: "600px",
-    content: "식품 유형을 입력해 주세요. (ex) 포장육",
-  },
-];
-
 const InfoNotice = () => {
   const [count, setCount] = useState(1);
+  const [form, setForm] = useState([]);
 
   const onCountHandle = () => {
-    setCount({ ...count, count: count + 1 });
+    setCount((prev) => prev + 1);
   };
 
+  const addNotice = (notice) => {
+    setForm(notice);
+    console.log(form);
+  };
   return (
     <InfoContainer>
       <InfoNoticeForm
-        infoDic={infoDic}
         count={count}
         onCountHandle={onCountHandle}
+        // addNotice={(notice) => console.log(notice)}
+        addNotice={addNotice}
       />
-      <AddButton>+ 정보고시 추가</AddButton>
     </InfoContainer>
   );
 };
@@ -60,21 +31,11 @@ export default InfoNotice;
 const InfoContainer = styled.section`
   padding: 20px;
   border: 1px solid black;
-  width: 1200px;
-  height: 900px;
+  width: 100%;
+  height: 100%;
   background-color: ${PALLETS.LIGHTGRAY};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const AddButton = styled.button`
-  width: ${(props) => props.width || "100%"};
-  border: 1px solid ${PALLETS.NAVY};
-  height: 50px;
-  text-align: center;
-  color: ${PALLETS.NAVY};
-  background-color: ${PALLETS.LIGHTGRAY};
-  font-size: 18px;
 `;
--- src/components/infoNotice/infoNoticeForm/InfoNoticeForm.jsx
+++ src/components/infoNotice/infoNoticeForm/InfoNoticeForm.jsx
@@ -1,74 +1,122 @@
-import React from "react";
-import { Button } from "components/common/Button";
+import React, { useState } from "react";
 import { Input } from "components/common/Input";
+
 import styled from "styled-components";
 import { PALLETS } from "style/theme";
 import InfoNoticeOption from "../infoNoticeOption/InfoNoticeOption";
-import { useState } from "react/cjs/react.development";
 
-/*"informationnotice" : {
-							"productname": String,
-							"origin": String,
-							"ranking" : String,
-							"custody" : String,
-							"foodcode" : String,
-							"key" : String,
-						}*/
+const infoDic = [
+  {
+    id: 1,
+    name: "name",
+    title: "제품명 / 중량",
+    placeholder: "제품명/중량을 입력해 주세요.",
+  },
+  {
+    id: 2,
+    name: "origin",
+    title: "원산지 / 원재료 함량",
+    placeholder: "원산지/원재 함량을 입력해 주세요.",
+  },
+  {
+    id: 3,
+    name: "ranking",
+    title: "등급",
+    placeholder: "등급 (근내지방도 수치)를 입력해 주세요.",
+  },
+  {
+    id: 4,
+    title: "보관",
+    name: "keep",
+    placeholder: "보관 방식을 입력해주세요",
+  },
+  {
+    id: 5,
+    title: "식품 유형",
+    name: "type",
+    placeholder: "식품 유형을 입력해 주세요. (ex) 포장육",
+  },
+];
 
-const InfoNoticeForm = ({ infoDic, count, onCountHandle }) => {
-  const [value, setValue] = useState("");
-  const [optionList, setOptionList] = useState([
+const InfoNoticeForm = ({ count, onCountHandle, addNotice }) => {
+  const [infoForm, setInfoForm] = useState({
+    name: "",
+    origin: "",
+    ranking: "",
+    keep: "",
+    type: "",
+  });
+
+  const [optionList, setOptionList] = useState(infoDic);
+
+  const onAdd = (newOption) => {
+    /*
     {
-      title: "항목 제목 자유 입력",
-      contents: "내용을 입력해주세요",
-    },
-  ]);
-
-  const addOption = ({ title, contents }) => {
-    const newOption = [...optionList, { title: title, contents: contents }];
-    setOptionList(newOption);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-
-    if (e.currentTarget.name === "title") {
-      addOption({ title: e.currentTarget.name });
-    } else addOption({ contents: e.currentTarget.name });
-
-    addOption(value);
-    setValue("");
+      id: 5,
+      title: "식품 유형",
+      name: "type", // 추가한 항목의 name은 어떻게?
+      content: "식품 유형을 입력해 주세요. (ex) 포장육",
+    }
+    */
+    setOptionList((prev) => [
+      ...prev,
+      {
+        ...newOption,
+        name: newOption.title.replace(/ /g, "-"), // slugify cjk-slug
+        id: prev.length + 1, // TODO: id 중복을 해결!
+      },
+    ]);
   };
 
   return (
-    <Container onChange={onCountHandle}>
-      <Form>
-        <h1>정보고시 {count}</h1>
-        {infoDic.map((item) => (
-          <InfoBox key={item.length}>
-            <label>{item.title} </label>
-            <Input width={item.width} placeholder={item.content} />
-          </InfoBox>
-        ))}
+    <>
+      <Container>
+        <Form
+          id="new-notice-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onCountHandle();
+            // 추가 로직!
+            addNotice(infoForm);
+            setInfoForm({
+              name: "",
+              origin: "",
+              ranking: "",
+              keep: "",
+              type: "",
+            });
+            setOptionList(infoDic);
+          }}
+        >
+          <h1>정보고시 {count}</h1>
+          {/* 기본 입력 form */}
+          {optionList.map((item, index) => (
+            <InfoBox key={item.id}>
+              <label htmlFor={item.name + "-input"}>{item.title}</label>
+              <Input
+                id={item.name + "-input"}
+                width="600px"
+                placeholder={item.placeholder}
+                value={infoForm[item.name]}
+                onChange={(e) => {
+                  setInfoForm((prev) => ({
+                    ...prev,
+                    [item.name]: e.target.value,
+                  }));
+                }}
+              />
+            </InfoBox>
+          ))}
+          {/* 객체를 보고 싶으면 => {JSON.stringify(infoForm)} 로 확인해보자. */}
+        </Form>
+        {/* 옵션 */}
+        <InfoNoticeOption onAdd={onAdd} />
+      </Container>
 
-        <InfoNoticeOption
-          optionList={optionList}
-          addOption={addOption}
-          value={value}
-          setValue={setValue}
-        />
-        <div onSubmit={handleSubmit}>
-          <Button
-            text={"+ 항목 추가"}
-            width={100}
-            bdcolor={PALLETS.BEIGE}
-            bgcolor={PALLETS.WHITE}
-            ftcolor={PALLETS.NAVY}
-          />
-        </div>
-      </Form>
-    </Container>
+      <AddButton type="submit" form="new-notice-form">
+        + 정보고시 추가
+      </AddButton>
+    </>
   );
 };
 
@@ -82,6 +130,15 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+const AddButton = styled.button`
+  width: ${(props) => props.width || "100%"};
+  border: 1px solid ${PALLETS.NAVY};
+  height: 50px;
+  text-align: center;
+  color: ${PALLETS.NAVY};
+  background-color: ${PALLETS.LIGHTGRAY};
+  font-size: 18px;
 `;
 
 const Form = styled.form`
