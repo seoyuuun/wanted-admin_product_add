08a6ab272e2a09924cd782542e2dc2b1d63fec71
JiyeonMin <ichbinmin2@gmail.com>
2022-01-29 01:46:23 +0900

    Add : InfoNotice Component '정보고시 추가' 기능 추가

--- src/components/infoNotice/InfoNotice.jsx
+++ src/components/infoNotice/InfoNotice.jsx
@@ -1,27 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InfoNoticeForm from "./infoNoticeForm/InfoNoticeForm";
 import { PALLETS } from "style/theme";
 import styled from "styled-components";
 const InfoNotice = () => {
-  const [count, setCount] = useState(1);
-  const [form, setForm] = useState([]);
+  const [count, setCount] = useState(2);
+  const [form, setForm] = useState([{ count: 1 }]);
 
-  const onCountHandle = () => {
+  const addNotice = (newNotice) => {
     setCount((prev) => prev + 1);
-  };
-
-  const addNotice = (notice) => {
-    setForm(notice);
+    setForm((prev) => [...prev, { ...newNotice, count }]);
     console.log(form);
   };
+
+  console.log(JSON.stringify(form));
+  useEffect(() => {}, []); // count가 있는 경우와 없는 경우를 생각하기
+
   return (
     <InfoContainer>
-      <InfoNoticeForm
-        count={count}
-        onCountHandle={onCountHandle}
-        // addNotice={(notice) => console.log(notice)}
-        addNotice={addNotice}
-      />
+      {form.map((notice) => (
+        <InfoNoticeForm
+          key={notice.count}
+          count={notice.count}
+          notice={notice}
+          setNotice={(transform) => {
+            setForm((oldForm) =>
+              oldForm.map((oldNotice) => {
+                if (oldNotice.count !== notice.count) return oldNotice;
+                return transform(oldNotice);
+              })
+            );
+          }}
+        />
+      ))}
+      <AddButton
+        onClick={() => {
+          addNotice();
+        }}
+      >
+        + 정보고시 추가
+      </AddButton>
     </InfoContainer>
   );
 };
@@ -38,4 +55,14 @@ const InfoContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const AddButton = styled.button`
+  width: ${(props) => props.width || "100%"};
+  border: 1px solid ${PALLETS.NAVY};
+  height: 50px;
+  text-align: center;
+  color: ${PALLETS.NAVY};
+  background-color: ${PALLETS.LIGHTGRAY};
+  font-size: 18px;
 `;
--- src/components/infoNotice/infoNoticeOption/InfoNoticeOption.jsx
+++ src/components/infoNotice/infoNoticeOption/InfoNoticeOption.jsx
@@ -7,7 +7,7 @@ import styled from "styled-components";
 import { PALLETS } from "style/theme";
 import { useState } from "react/cjs/react.development";
 
-const InfoNoticeOption = ({ onAdd }) => {
+const InfoNoticeOption = ({ onAdd, onDel, count }) => {
   const [optionForm, setOptionForm] = useState({
     title: "",
     placeholder: "",
@@ -24,12 +24,9 @@ const InfoNoticeOption = ({ onAdd }) => {
   const { title, placeholder } = optionForm;
 
   const onSubmitInput = (e) => {
-    console.log("info 제출");
     e.preventDefault();
     if (!title || !placeholder) return;
-
     onAdd(optionForm);
-
     setOptionForm({
       title: "",
       placeholder: "",
@@ -38,31 +35,35 @@ const InfoNoticeOption = ({ onAdd }) => {
 
   return (
     <>
-      <InfoBox id="new-option-form" onSubmit={onSubmitInput}>
-        <Input
-          width="30%"
-          placeholder="항목 제목 자유 입력"
-          name="title"
-          value={title}
-          onChange={onChangeInput}
-        />
-        <Input
-          width="50%"
-          placeholder="내용을 입력해주세요"
-          name="placeholder"
-          value={placeholder}
-          onChange={onChangeInput}
-        />
+      <div style={{ display: "flex" }}>
+        <InfoBox id={"new-option-form-" + count} onSubmit={onSubmitInput}>
+          <Input
+            width="30%"
+            placeholder="항목 제목 자유 입력"
+            name="title"
+            value={title}
+            onChange={onChangeInput}
+          />
+          <Input
+            width="50%"
+            placeholder="내용을 입력해주세요"
+            name="placeholder"
+            value={placeholder}
+            onChange={onChangeInput}
+          />
+        </InfoBox>
         <Button
           text="삭제"
           width="100px"
           bdcolor={PALLETS.RED}
           bgcolor={PALLETS.WHITE}
           ftcolor={PALLETS.NAVY}
+          onClick={onDel}
         />
-      </InfoBox>
+      </div>
+
       <Button
-        form="new-option-form"
+        form={"new-option-form-" + count}
         type="submit"
         text="+ 항목 추가"
         width={100}
