3c1565b2ce0727e129c29a419a01cbb0709566fa
JiyeonMin <ichbinmin2@gmail.com>
2022-01-28 21:26:55 +0900

    Add : 상품 정보 고시 항목 추가 기능 추가

--- src/components/infoNotice/infoNoticeOption/InfoNoticeOption.jsx
+++ src/components/infoNotice/infoNoticeOption/InfoNoticeOption.jsx
@@ -1,42 +1,75 @@
-import React, { useRef } from "react";
+import React from "react";
+
 import { Input } from "components/common/Input";
+import { Button } from "components/common/Button";
+
 import styled from "styled-components";
+import { PALLETS } from "style/theme";
 import { useState } from "react/cjs/react.development";
 
-const InfoNoticeOption = ({ value, optionList, setValue }) => {
-  const formRef = useRef();
-  const inputRef = useRef();
+const InfoNoticeOption = ({ onAdd }) => {
+  const [optionForm, setOptionForm] = useState({
+    title: "",
+    placeholder: "",
+  });
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const name = inputRef.current.value;
-    console.log(name);
-    // name && props.onAdd(name);
-    formRef.current.reset();
+  const onChangeInput = (e) => {
+    const { value, name } = e.target;
+    setOptionForm({
+      ...optionForm,
+      [name]: value,
+    });
+  };
+
+  const { title, placeholder } = optionForm;
+
+  const onSubmitInput = (e) => {
+    console.log("info 제출");
+    e.preventDefault();
+    if (!title || !placeholder) return;
+
+    onAdd(optionForm);
+
+    setOptionForm({
+      title: "",
+      placeholder: "",
+    });
   };
 
   return (
     <>
-      {optionList.map((item) => (
-        <InfoBox ref={formRef}>
-          <Input
-            ref={inputRef}
-            width={"350px"}
-            placeholder={item.title}
-            name="title"
-            // onChange={(e) => setValue(e.target.value)}
-            onChange={onSubmit}
-          />
-          <Input
-            ref={inputRef}
-            width={"600px"}
-            placeholder={item.contents}
-            name="contents"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </InfoBox>
-      ))}
+      <InfoBox id="new-option-form" onSubmit={onSubmitInput}>
+        <Input
+          width="30%"
+          placeholder="항목 제목 자유 입력"
+          name="title"
+          value={title}
+          onChange={onChangeInput}
+        />
+        <Input
+          width="50%"
+          placeholder="내용을 입력해주세요"
+          name="placeholder"
+          value={placeholder}
+          onChange={onChangeInput}
+        />
+        <Button
+          text="삭제"
+          width="100px"
+          bdcolor={PALLETS.RED}
+          bgcolor={PALLETS.WHITE}
+          ftcolor={PALLETS.NAVY}
+        />
+      </InfoBox>
+      <Button
+        form="new-option-form"
+        type="submit"
+        text="+ 항목 추가"
+        width={100}
+        bdcolor={PALLETS.BEIGE}
+        bgcolor={PALLETS.WHITE}
+        ftcolor={PALLETS.NAVY}
+      />
     </>
   );
 };
