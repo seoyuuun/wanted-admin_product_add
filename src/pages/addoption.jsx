8e0a9f7daf2d735a6140aa15ff98525bb26103dc
seoyoung park <ongddree@gmail.com>
2022-01-28 19:25:47 +0900

    Style : 상품 옵션 UI

--- src/pages/addoption.jsx
+++ src/pages/addoption.jsx
@@ -1,14 +1,23 @@
 import Contentwrap from 'components/layout/Contentwrap';
+import styled from 'styled-components';
+import { Mainoption } from 'components/productoption';
+import { PALLETS } from 'style/theme';
 
 const Addoption = () => {
   return (
     <Contentwrap title="상품 옵션">
       <Contentwrap.Main>
-        <h2>여기서 작업해주세요.</h2>
-        <div>작업공간입니다 작업공간입니다 작업공간입니다 작업공간입니다</div>
+        <Wrap>
+          <Mainoption />
+        </Wrap>
       </Contentwrap.Main>
     </Contentwrap>
   );
 };
 
 export default Addoption;
+
+const Wrap = styled.section`
+  background: ${PALLETS.LIGHTGRAY};
+  padding: 20px;
+`;
