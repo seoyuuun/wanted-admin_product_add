beca3fe008bf98a2abfdbfec8fba432723068768
JiyeonMin <ichbinmin2@gmail.com>
2022-01-28 12:41:49 +0900

    Add : admin page에 개인 파일 생성 후 추가

---
+++ src/pages/admin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Jy from "./jy";
+
+const Admin = (props) => {
+  return (
+    <>
+      <Jy />
+    </>
+  );
+};
+
+export default Admin;
---
+++ src/pages/jy.jsx
@@ -0,0 +1,8 @@
+import InfoNotice from "components/infoNotice/InfoNotice";
+import React from "react";
+
+const Jy = (props) => {
+  return <InfoNotice />;
+};
+
+export default Jy;
