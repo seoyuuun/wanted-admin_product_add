a60bf2868c263689bad599663e17e601a5a1a375
seoyoung park <ongddree@gmail.com>
2022-01-28 19:25:03 +0900

    Style : desc 옵션 추가

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,12 +1,24 @@
-import styled from "styled-components";
-import { PALLETS } from "style/theme";
+import styled from 'styled-components';
+import { PALLETS } from 'style/theme';
 
-export const Input = ({ width, placeholder }) => {
-  return <Container type="text" width={width} placeholder={placeholder} />;
+export const Input = ({ width, placeholder, desc }) => {
+  return (
+    <Container>
+      <Inputspace type="text" width={width} placeholder={placeholder} />
+      <Desc display={desc}>{desc}</Desc>
+    </Container>
+  );
 };
 
-const Container = styled.input`
-  width: ${(props) => props.width || "100%"};
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-wrap: no-wrap;
+`;
+
+const Inputspace = styled.input`
+  width: ${(props) => props.width || '100%'};
   border: 1px solid ${PALLETS.LIGHTGRAY};
   border-radius: 8px;
   padding: 0 15px;
@@ -15,4 +27,9 @@ const Container = styled.input`
     color: ${PALLETS.GRAY};
     font-size: 15px;
   }
+`;
+
+const Desc = styled.span`
+  display: ${(props) => props.display || 'none'};
+  margin-left: 10px;
 `;
