79acd98f0f84af53888487c85f45f2b130c97ffd
seoyoung park <ongddree@gmail.com>
2022-01-29 16:34:43 +0900

    Feat : 옵션 세트 내 옵션 추가

--- src/components/productoption/Optionset.jsx
+++ src/components/productoption/Optionset.jsx
@@ -5,30 +5,30 @@ import { Suboption } from './Suboption';
 import { useState, useRef } from 'react';
 import InsertImagePreview from 'components/common/InsertImagePreview';
 
-export const Optionset = ({ optionSets, onSetRemove }) => {
-  const [options, setOptions] = useState([]);
+export const Optionset = ({ optionSets, onSetRemove, nextId }) => {
+  const [options, setOptions] = useState([{ id: 0 }]);
   const [subOptions, setSubOptions] = useState([]);
 
-  const nextOptId = useRef(0);
+  const nextOptId = useRef(1);
   const nextSubId = useRef(0);
 
   const onOptCreate = () => {
     const list = {
-      id: nextSubId.current,
+      id: `${nextId}-${nextOptId.current}`,
     };
-    setOptions(subOptions.concat(list));
+    setOptions(options.concat(list));
     nextOptId.current += 1;
   };
 
   const onOptRemove = (e) => {
     let optArr = [...options];
     console.log(optArr);
-    setSubOptions(subOptions.filter((list) => list.id !== e));
+    setOptions(options.filter((list) => list.id !== e));
   };
 
   const onSubCreate = () => {
     const list = {
-      id: nextSubId.current,
+      id: `${nextId}-${nextOptId.current}-${nextSubId.current}`,
     };
     setSubOptions(subOptions.concat(list));
     nextSubId.current += 1;
@@ -36,7 +36,6 @@ export const Optionset = ({ optionSets, onSetRemove }) => {
 
   const onSubRemove = (e) => {
     let subArr = [...subOptions];
-    console.log(subArr);
     setSubOptions(subOptions.filter((list) => list.id !== e));
   };
 
@@ -55,48 +54,62 @@ export const Optionset = ({ optionSets, onSetRemove }) => {
               />
             </Header>
             <Main>
-              <Attachimage>이미지첨부</Attachimage>
-              <Writesec>
-                <Writeheader>
-                  <Button
-                    text={'삭제'}
-                    width={'50px'}
-                    bdcolor={`${PALLETS.RED}`}
-                    ftcolor={`${PALLETS.RED}`}
-                  />
-                </Writeheader>
-                <Writemain>
-                  <Input placeholder={'옵션명을 입력해주세요. (필수)'}></Input>
-                  <Inputsec>
-                    <Input
-                      placeholder={'상품 정상가 (필수)'}
-                      desc={'원'}
-                    ></Input>
-                    <Discountrate>
-                      &#123;&#123;할인율 &#125;&#125; &#37;
-                    </Discountrate>
-                    <Input
-                      placeholder={'상품 판매가 (필수)'}
-                      desc={'원'}
-                    ></Input>
-                    <Input placeholder={'재고 (필수)'} desc={'개'}></Input>
-                    <Input placeholder={'비과세'}></Input>
-                  </Inputsec>
-                  <Suboption
-                    subOptions={subOptions}
-                    onSubRemove={onSubRemove}
-                  />
-                  <Btnaddsub>
-                    <Btnaddsubtit onClick={onSubCreate}>
-                      추가 옵션 상품 추가
-                    </Btnaddsubtit>
-                  </Btnaddsub>
-                </Writemain>
-              </Writesec>
+              <InsertImagePreview />
+              {options &&
+                options.map((e) => (
+                  <Writesec>
+                    <Writeheader>
+                      <Button
+                        text={'삭제'}
+                        width={'50px'}
+                        bdcolor={`${PALLETS.RED}`}
+                        ftcolor={`${PALLETS.RED}`}
+                        onClick={() => onOptRemove(e.id)}
+                      />
+                    </Writeheader>
+                    <Writemain>
+                      <Input
+                        placeholder={'옵션명을 입력해주세요. (필수)'}
+                        type="text"
+                      ></Input>
+                      <Inputsec>
+                        <Input
+                          placeholder={'상품 정상가 (필수)'}
+                          desc={'원'}
+                          type="number"
+                        ></Input>
+                        <Discountrate>
+                          &#123;&#123;할인율 &#125;&#125; &#37;
+                        </Discountrate>
+                        <Input
+                          placeholder={'상품 판매가 (필수)'}
+                          desc={'원'}
+                          type="number"
+                        ></Input>
+                        <Input
+                          placeholder={'재고 (필수)'}
+                          desc={'개'}
+                          type="number"
+                        ></Input>
+                        <Input placeholder={'비과세'}></Input>
+                      </Inputsec>
+                      <Suboption
+                        subOptions={subOptions}
+                        onSubRemove={onSubRemove}
+                      />
+                      <Btnaddsub>
+                        <Btnaddsubtit onClick={onSubCreate}>
+                          추가 옵션 상품 추가
+                        </Btnaddsubtit>
+                      </Btnaddsub>
+                    </Writemain>
+                  </Writesec>
+                ))}
               <Button
                 text={'+옵션 추가'}
                 bdcolor={`${PALLETS.NAVY}`}
                 ftcolor={`${PALLETS.NAVY}`}
+                onClick={onOptCreate}
               />
             </Main>
           </Sectionwrap>
