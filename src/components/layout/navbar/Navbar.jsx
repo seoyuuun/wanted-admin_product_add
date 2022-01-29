9a898466b37a70a970788ff06908af1ff0706124
seoyuun <carpedm1221@gmail.com>
2022-01-29 14:33:49 +0900

    Add: navBar '저장하기' 버튼 추가, 선주문 토글값 useState로 저장

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { PALLETS } from 'style/theme';
+import { Button } from 'components/common/Button';
 
 const Navbar = () => {
   return (
     <NavbarWrapper>
       <span>상품등록</span>
+      <Button
+        text={'저장하기'}
+        width={'100px'}
+        bd={`${PALLETS.LIGHTPURPLE}`}
+        ft={`${PALLETS.BLACK}`}
+      />
     </NavbarWrapper>
   );
 };
@@ -13,11 +20,15 @@ const Navbar = () => {
 export default Navbar;
 
 const NavbarWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
   height: 53.5px;
   border: 2px solid ${PALLETS.LIGHTGRAY};
   color: ${PALLETS.BLACK};
   line-height: 50px;
   padding-left: 280px;
+  padding-right: 20px;
   font-weight: bold;
 `;
