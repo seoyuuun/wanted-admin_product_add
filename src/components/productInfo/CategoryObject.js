f2a122fbdf78e09e9d2b1e54a668c7ef86fbcb82
dahye-program <dahye0729@sch.ac.kr>
2022-01-29 16:40:23 +0900

    Add : Category Object 추가

---
+++ src/components/productInfo/CategoryObject.js
@@ -0,0 +1,32 @@
+export const CategoryData = [
+    {
+        id: 0,
+        name: "카테고리1",
+        value: false
+    },
+    {
+        id: 1,
+        name: "카테고리2",
+        value: false
+    },
+    {
+        id: 2,
+        name: "카테고리3",
+        value: false
+    },
+    {
+        id: 3,
+        name: "카테고리4",
+        value: false
+    },
+    {
+        id: 4,
+        name: "카테고리5",
+        value: false
+    },
+    {
+        id: 5,
+        name: "카테고리6",
+        value: false
+    },
+];
